5e105707d8f0bc75864bf605553b97209097d851
Muhammad Magdy <muh.magdy1@gmail.com>
2024-05-22 23:12:33 +0300

    Added LZW support

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import technique.FileEncoding;
 import technique.arithmetic.ArithmeticEncoding;
 import technique.huffman.HuffmanEncoding;
+import technique.lzw.LZWEncoding;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,20 +10,21 @@ public class Main {
     private static final Map<String, FileEncoding> encodings = new HashMap<>() {{
         put("h", new HuffmanEncoding());
         put("a", new ArithmeticEncoding());
+        put("l", new LZWEncoding());
     }};
 
     public static void main(String[] args) {
         if(args.length != 4) {
             System.out.println(
                     "Usage: encoding.jar algorithm c|d <input file name> <output file name>\n" +
-                            "algorithm: h for Huffman, a for Arithmetic\n" +
+                            "algorithm: h for Huffman, a for Arithmetic, l for LZW\n" +
                             "c for compress, d for decompress"
             );
             return;
         }
 
-        String algorithm = args[0].substring(0, 1);
-        String mode = args[1];
+        String algorithm = args[0].substring(0, 1).toLowerCase();
+        String mode = args[1].toLowerCase();
         String inputFileName = args[2];
         String outputFileName = args[3];
 
